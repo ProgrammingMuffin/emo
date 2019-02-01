8012f26659dd313e854155434a7ec20d43b101fa
Deepak Ramachandran <42649107+ProgrammingMuffin@users.noreply.github.com>
2019-02-02 00:12:55 +0700

    testing
    testing uploads

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$file_name = $_FILES['cam']['name'];
+$file_tmpname = $_FILES['']['tmp_name'];
+$file_size = $_FILES['cam']['size'];
+
+echo "file name: " . $file_name . "<br>";
+echo "file temporary name: " . $file_tmpname . "<br>";
+echo "file size: " . $file_size . "<br>";
+
+?>
+
+<html>
+<body>
+<h1>Front camera unit testing</h1>
+<form enctype="multipart/form-data" action="index.php" method="post" >
+<input type="file" name="cam" accept="images/*" capture="user" >
+<input type="submit" value="scan" >
+</form>
+</body>
+</html>
