18625e3cf28d9308106a743413db52ee65db3cae
Deepak Ramachandran <42649107+ProgrammingMuffin@users.noreply.github.com>
2019-02-14 01:47:52 +0700

    added landing page
    added a very simplistic landing page.
    
    more features have to be added soon.

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
-<?php
-
-$file_name = $_FILES['cam']['name'];
-$file_tmpname = $_FILES['cam']['tmp_name'];
-$file_size = $_FILES['cam']['size'];
-
-echo "file name: " . $file_name . "<br>";
-echo "file temporary name: " . $file_tmpname . "<br>";
-echo "file size: " . $file_size . "<br>";
-
-echo '<img src="' . $file_tmpname . '" >';
-
-?>
-
 <html>
+<head>
+<link href="stylesheets/index.css" type="text/css" rel="stylesheet" />
+<meta name="viewport" content="width=device-width, initial-scale=1" >
+</head>
 <body>
-<h1>Front camera unit testing</h1>
-<form enctype="multipart/form-data" action="index.php" method="post" >
-<input type="file" name="cam" accept="images/*" capture="user" >
-<input type="submit" value="scan" >
-</form>
+	<div class="topbar_container" >
+		<div class="topbar" >
+			<img class="topbar_logo" src="images/logo.png" >
+		</div>
+	</div>
+	<div class="intro_container" >
+		<div class="intro" >
+			<div class="intro_head" >Welcome to Emo</div>
+			<div class="intro_section_container" >
+				<div class="intro_left" ><br><br>A web based music player that suggests you music based on your emotions<br><br>Let your emotions control the music.</div>
+				<div class="intro_left" >
+					<img class="intro_img" src="images/phone.png" >
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
