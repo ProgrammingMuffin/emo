b5463b2e5377c6993e418fe0989bbdca0991784a
Deepak Ramachandran <42649107+ProgrammingMuffin@users.noreply.github.com>
2019-02-14 02:01:06 +0700

    test
    test

--- index.php
+++ index.php
@@ -13,7 +13,8 @@
 		<div class="intro" >
 			<div class="intro_head" >Welcome to Emo</div>
 			<div class="intro_section_container" >
-				<div class="intro_left" ><br><br>A web based music player that suggests you music based on your emotions<br><br>Let your emotions control the music.</div>
+				<div class="intro_left" ><br><br>A web based music player that suggests you music based on your emotions
+				<br><br>Let your emotions control the music.</div>
 				<div class="intro_left" >
 					<img class="intro_img" src="images/phone.png" >
 				</div>
